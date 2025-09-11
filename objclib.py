ec1692b03314bf1124f03e76cb8b14232308c7b6
Kazunori Baba <babacchi@gmail.com>
2025-09-12 02:23:45 +0900

    メニューを隠す方法を変更

--- objclib.py
+++ objclib.py
@@ -1,7 +1,7 @@
 import objc
-from AppKit import NSApplication, NSApplicationPresentationHideDock, NSApplicationPresentationHideMenuBar
+from AppKit import NSApplication, NSApplicationPresentationHideDock, NSApplicationPresentationAutoHideMenuBar
 
 def hide_menubar_and_dock():
     NSApp = NSApplication.sharedApplication()
-    options = NSApplicationPresentationHideDock | NSApplicationPresentationHideMenuBar
+    options = NSApplicationPresentationHideDock | NSApplicationPresentationAutoHideMenuBar
     NSApp.setPresentationOptions_(options)
